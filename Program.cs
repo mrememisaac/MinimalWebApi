a3f62b4050177b146be49bd68ebcd6b8ccefda73
Emem Isaac <me@ememisaac.com>
2022-05-09 23:00:02 +0100

    Registers the dbcontext, product repository and the list products endpoint

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using MinimalWebApi.Data;
+using MinimalWebApi.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(
+    o => o.UseQueryTrackingBehavior(Microsoft.EntityFrameworkCore.QueryTrackingBehavior.NoTracking));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/products", async (IProductRepository repository) =>
+{
+    var products = await repository.List();
+    return products.Select(p => p.Convert());
+})
+.WithName("GetProducts");
+
+app.Run();
+
+record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
