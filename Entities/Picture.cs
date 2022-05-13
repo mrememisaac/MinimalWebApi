684db84cda3520a309d71a3a66e5df8c996826eb
Emem Isaac <me@ememisaac.com>
2022-05-13 18:08:35 +0100

    Adds documentation

--- Entities/Picture.cs
+++ Entities/Picture.cs
@@ -2,20 +2,46 @@ namespace MinimalWebApi.Entities;
 
 public sealed class Picture : Entity
 {
-    public string Base64String { get; }
+    /// <summary>
+    /// Bas64 Encoded Image string
+    /// </summary>
+    /// <value></value>
+    public string Base64String { get; private set; }
 
-    public Guid ProductId { get; }
+    /// <summary>
+    /// The Product this image belongs to
+    /// </summary>
+    /// <value></value>
+    public Product Product { get; private set; }
 
-    private Picture(Guid id, string base64String, Guid productId) : base(id)
+    /// <summary>
+    /// Added to allow Entity Framework materialize instances
+    /// </summary>
+    private Picture() { }
+
+    /// <summary>
+    /// Constructor privatised to ensure Picture instance is created safely
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="base64String"></param>
+    /// <param name="product"></param>
+    private Picture(Guid id, string base64String, Product product)
     {
-        ProductId = productId;
+        Id = id;
+        Product = product;
         Base64String = base64String;
     }
 
-    public static Picture Create(Guid id, string base64String, Guid productId)
+    /// <summary>
+    /// Creates a Picture instance after all safety checks have passed
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="base64String"></param>
+    /// <param name="product"></param>
+    /// <returns></returns>
+    public static Picture Create(Guid id, string base64String, Product product)
     {
-        if (productId == Guid.Empty) throw new ArgumentException("ProductId must be a valid Guid", nameof(productId));
         if (String.IsNullOrWhiteSpace(base64String)) throw new ArgumentException("Parameter cannot be null, whitespace or empty string. It requires a valid base64 string", nameof(base64String));
-        return new Picture(id, base64String, productId);
+        return new Picture(id, base64String, product);
     }
 }
--- Entities/Product.cs
+++ Entities/Product.cs
@@ -2,29 +2,78 @@ namespace MinimalWebApi.Entities;
 
 public sealed class Product : Entity
 {
-    public string Name { get; }
+    /// <summary>
+    /// Product Name
+    /// </summary>
+    /// <value></value>
+    public string Name { get; private set; }
 
-    public string Description { get; }
+    /// <summary>
+    /// Product Description
+    /// </summary>
+    /// <value></value>
+    public string Description { get; private set; }
 
-    public int Price { get; }
+    /// <summary>
+    /// Product Price.
+    /// </summary>
+    /// <value></value>
+    public int Price { get; private set; }
 
-    private List<Picture> Pictures { get; } = new List<Picture>();
+    /// <summary>
+    /// Product Pictures
+    /// </summary>
+    /// <typeparam name="Picture"></typeparam>
+    /// <returns>List<Entities.Picture></returns>
+    private List<Picture> Pictures { get; set; } = new List<Picture>();
 
     public IReadOnlyCollection<Picture> PicturesCollection => Pictures.AsReadOnly();
 
-    private Product(Guid id, string name, string description, int price, IEnumerable<Picture> pictures) : base(id)
+    /// <summary>
+    /// Private constructor added to allow EF hydrate instances
+    /// </summary>
+    private Product() { }
+
+    /// <summary>
+    /// Constructor privatised to ensure can only be called by the Create static method after all safety checks have passed
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="name"></param>
+    /// <param name="description"></param>
+    /// <param name="price"></param>
+    private Product(Guid id, string name, string description, int price, string createdBy)
     {
+        Id = id;
         Name = name;
         Description = description;
         Price = price;
-        Pictures.AddRange(pictures);
+        CreatedBy = createdBy;
+        DateCreated = DateTimeOffset.Now;
     }
 
-    public static Product Create(Guid id, string name, string description, int price, IEnumerable<Picture> pictures)
+    /// <summary>
+    /// Creates a product instance after satisfying all safety checks.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="name"></param>
+    /// <param name="description"></param>
+    /// <param name="price"></param>
+    /// <returns>A Product if all goes well. Else throws an exception</returns>
+    public static Product Create(Guid id, string name, string description, int price, string createdBy)
     {
         if (String.IsNullOrWhiteSpace(name)) throw new ArgumentException("Provide a valid name", nameof(name));
         if (String.IsNullOrWhiteSpace(description)) throw new ArgumentException("Provide a valid description", nameof(description));
         if (price < 500) throw new ArgumentException("Price cannot be less than 500", nameof(price));
-        return new Product(id, name, description, price, pictures);
+        if (String.IsNullOrWhiteSpace(createdBy)) throw new ArgumentException("Creator username required", nameof(createdBy));
+        return new Product(id, name, description, price, createdBy);
+    }
+
+    /// <summary>
+    /// Adds a product picture
+    /// </summary>
+    /// <param name="picture"></param>
+    public void AddPicture(Picture picture)
+    {
+        Pictures.Add(picture);
     }
 }
