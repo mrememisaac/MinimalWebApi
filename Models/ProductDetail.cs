a0c51f4b47bfbe4871f883ade71d73467fccc4bc
Emem Isaac <me@ememisaac.com>
2023-06-10 18:51:28 +0100

    Product details dto

---
+++ Models/ProductDetail.cs
@@ -0,0 +1,16 @@
+﻿namespace ProductsApi.Models;
+
+public class ProductDetail
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public int Price { get; set; }
+
+    public string CreatedBy { get; set; } = string.Empty;
+
+    public List<Picture> Pictures { get; set; } = new List<Picture>();
+}
