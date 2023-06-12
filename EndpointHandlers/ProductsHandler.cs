3d1cf59a9be314dae9fd3bdbd1d697263606e4fc
Emem Isaac <me@ememisaac.com>
2023-06-12 22:26:53 +0100

    Adds logger and automapper

--- EndpointHandlers/ProductsHandler.cs
+++ EndpointHandlers/ProductsHandler.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using ProductsApi.Models;
 using ProductsApi.Repositories;
@@ -6,15 +7,19 @@ using System.Threading;
 
 namespace ProductsApi.EndpointHandlers
 {
-    public static class ProductsHandler
+    public class ProductsHandler
     {
-        public static async Task<Ok<IEnumerable<Product>>> GetProducts(IProductRepository repository, CancellationToken cancellationToken)
+        public static async Task<Ok<IEnumerable<Product>>> GetProducts(IProductRepository repository, 
+            CancellationToken cancellationToken,
+            IMapper mapper,
+            ILogger<ProductsHandler> logger)
         {
+            logger.LogInformation($"Getting Products");
             var products = await repository.List(cancellationToken: cancellationToken);
-            return TypedResults.Ok(products.Select(p => p.Convert()));
+            return TypedResults.Ok(mapper.Map<IEnumerable<Models.Product>>(products));
         }
 
-        public static async Task<Results<NotFound, NoContent>> DeleteProduct(Guid id, IProductRepository repository, CancellationToken cancellationToken)
+        public static async Task<Results<NotFound, NoContent>> DeleteProduct(Guid id, IProductRepository repository, CancellationToken cancellationToken, ILogger<ProductsHandler> logger)
         {
             var pdt = await repository.GetById(id, cancellationToken);
             if (pdt == null)
@@ -25,31 +30,43 @@ namespace ProductsApi.EndpointHandlers
             return TypedResults.NoContent();
         }
 
-        public static async Task<Results<NotFound, NoContent>> UpdateProduct([FromBody] ProductsApi.Models.Product product, IProductRepository repository, CancellationToken cancellationToken)
+        public static async Task<Results<NotFound, NoContent>> UpdateProduct(Guid id, [FromBody] UpdateProduct product, 
+            IProductRepository repository, 
+            CancellationToken cancellationToken, 
+            IMapper mapper,
+            ILogger<ProductsHandler> logger)
         { 
-            var pdt = await repository.GetById(product.Id, cancellationToken);
+            var pdt = await repository.GetById(id, cancellationToken);
             if(pdt == null)
             {
                 return TypedResults.NotFound();
             }
-            await repository.Update(product.Convert(), cancellationToken);
+            await repository.Update(mapper.Map<Entities.Product>(pdt), cancellationToken);
             return TypedResults.NoContent();
         }
 
-        public static async Task<CreatedAtRoute<Product>> CreateProduct([FromBody] ProductsApi.Models.Product product, IProductRepository repository, CancellationToken cancellationToken)
+        public static async Task<CreatedAtRoute<Product>> CreateProduct([FromBody] CreateProduct product, 
+            IProductRepository repository, 
+            CancellationToken cancellationToken,
+            IMapper mapper,
+            ILogger<ProductsHandler> logger)
         {
-            var newProduct = await repository.Add(product.Convert(), cancellationToken);
-            return TypedResults.CreatedAtRoute(newProduct.Convert(), "GetProduct", new { id= newProduct.Id });
+            var entityProduct = await repository.Add(mapper.Map<Entities.Product>(product), cancellationToken);
+            return TypedResults.CreatedAtRoute(mapper.Map<Product>(entityProduct), "GetProduct", new { id= entityProduct.Id });
         }
 
-        public static async Task<Results<NotFound, Ok<Product>>> GetProduct(Guid id, IProductRepository repository, CancellationToken cancellationToken)
+        public static async Task<Results<NotFound, Ok<Product>>> GetProduct(Guid id, 
+            IProductRepository repository, 
+            CancellationToken cancellationToken, 
+            IMapper mapper,
+            ILogger<ProductsHandler> logger)
         {
             var product = await repository.GetById(id, cancellationToken);
             if (product == null)
             {
                 return TypedResults.NotFound();
             }
-            return TypedResults.Ok(product.Convert());
+            return TypedResults.Ok(mapper.Map<Models.Product>(product));
         }
     }
 }
