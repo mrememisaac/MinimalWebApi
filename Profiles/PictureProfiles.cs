6d5b37cb98aec9eda1c75d20b5fbeddd9e9d7e2a
Emem Isaac <me@ememisaac.com>
2023-06-12 22:31:28 +0100

    Adds mapping profiles

---
+++ Profiles/PictureProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ProductsApi.Models;
+
+namespace ProductsApi.Profiles
+{
+    public class PictureProfiles : Profile
+    {
+        public PictureProfiles()
+        {
+            CreateMap<Picture, Entities.Picture>().ReverseMap();
+            CreateMap<CreatePicture, Entities.Picture>().ReverseMap();
+            CreateMap<UpdatePicture, Entities.Picture>().ReverseMap();
+        }
+    }
+}
---
+++ Profiles/ProductProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ProductsApi.Models;
+
+namespace ProductsApi.Profiles
+{
+    public class ProductProfiles : Profile
+    {
+        public ProductProfiles() 
+        { 
+            CreateMap<Product, Entities.Product>().ReverseMap();
+            CreateMap<CreateProduct, Entities.Product>().ReverseMap();
+            CreateMap<UpdateProduct, Entities.Product>().ReverseMap();
+
+        }
+    }
+}
