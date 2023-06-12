2321d40de065722c9ad742d94c56d99e0542eb46
Emem Isaac <me@ememisaac.com>
2023-06-12 22:25:58 +0100

    Adds validation filters for create and update endpoints

---
+++ EndpointFilters/CreateProductAnnotationsValidationFilter.cs
@@ -0,0 +1,18 @@
+﻿using MiniValidation;
+using ProductsApi.Models;
+
+namespace ProductsApi.EndpointFilters
+{
+    public class CreateProductAnnotationsValidationFilter : IEndpointFilter
+    {
+        public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+        {
+            var model = context.GetArgument<CreateProduct>(1);
+            if (MiniValidator.TryValidate(model, out var validationErrors))
+            {
+                return TypedResults.ValidationProblem(validationErrors);
+            }
+            return await next(context);
+        }
+    }
+}
---
+++ EndpointFilters/UpdateProductAnnotationsValidationFilter.cs
@@ -0,0 +1,18 @@
+﻿using MiniValidation;
+using ProductsApi.Models;
+
+namespace ProductsApi.EndpointFilters
+{
+    public class UpdateProductAnnotationsValidationFilter : IEndpointFilter
+    {
+        public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+        {
+            var model = context.GetArgument<UpdateProduct>(2);
+            if (MiniValidator.TryValidate(model, out var validationErrors))
+            {
+                return TypedResults.ValidationProblem(validationErrors);
+            }
+            return await next(context);
+        }
+    }
+}
