8ad3f84078b639999eef3d928ba8b52e0527deea
Emem Isaac <me@ememisaac.com>
2023-06-12 17:53:13 +0100

    Adds use of endpoint extension

---
+++ Extensions/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,40 @@
+﻿using ProductsApi.EndpointHandlers;
+
+namespace ProductsApi.Extensions
+{
+    public static class EndpointRouteBuilderExtensions
+    {
+        public static void RegisterProductEndpoints(this IEndpointRouteBuilder endpointRouteBuilder)
+        {
+            var productsEndpoints = endpointRouteBuilder.MapGroup("/products");
+            var productsWithGuidEndpoints = endpointRouteBuilder.MapGroup("/products/{id:guid}");
+
+            productsEndpoints.MapGet("", ProductsHandler.GetProducts)
+                .Produces<IEnumerable<ProductsApi.Models.Product>>(StatusCodes.Status200OK)
+                .WithName("GetProducts");
+
+            productsWithGuidEndpoints.MapDelete("", ProductsHandler.DeleteProduct)
+                .Produces<ProductsApi.Models.Product>(StatusCodes.Status500InternalServerError)
+                .Produces<ProductsApi.Models.Product>(StatusCodes.Status400BadRequest)
+                .Produces<ProductsApi.Models.Product>(StatusCodes.Status204NoContent)
+                .WithName("DeleteProduct");
+
+            productsWithGuidEndpoints.MapPut("", ProductsHandler.UpdateProduct)
+                .Produces<ProductsApi.Models.Product>(StatusCodes.Status500InternalServerError)
+                .Produces<ProductsApi.Models.Product>(StatusCodes.Status400BadRequest)
+                .Produces<ProductsApi.Models.Product>(StatusCodes.Status204NoContent)
+                .WithName("UpdateProduct");
+
+            productsEndpoints.MapPost("", ProductsHandler.CreateProduct)
+                .Produces<ProductsApi.Models.Product>(StatusCodes.Status500InternalServerError)
+                .Produces<ProductsApi.Models.Product>(StatusCodes.Status400BadRequest)
+                .Produces<ProductsApi.Models.Product>(StatusCodes.Status201Created)
+                .WithName("CreateProduct");
+
+            productsWithGuidEndpoints.MapGet("", ProductsHandler.GetProduct)
+                .Produces<ProductsApi.Models.ProductDetail>(StatusCodes.Status200OK)
+                .Produces<ProductsApi.Models.ProductDetail>(StatusCodes.Status404NotFound)
+                .WithName("GetProduct");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using ProductsApi.Data;
 using ProductsApi.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using ProductsApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,65 +28,6 @@ app.UseCors(config => config.WithOrigins("http://localhost:4000")
 
 app.UseHttpsRedirection();
 
-
-app.MapGet("/products", async (IProductRepository repository, CancellationToken cancellationToken) =>
-{
-    var products = await repository.List(cancellationToken: cancellationToken);
-    return Results.Ok(products.Select(p => p.Convert()));
-})
-.Produces<IEnumerable<ProductsApi.Models.Product>>(StatusCodes.Status200OK)
-.WithName("GetProducts");
-
-app.MapGet("/products/{id:Guid}", async (Guid id, IProductRepository repository, CancellationToken cancellationToken) =>
-{
-    var product = await repository.GetById(id, cancellationToken);
-    if (product == null)
-    {
-        return Results.NotFound();
-    }
-    return Results.Ok(product.Convert());
-})
-.Produces<ProductsApi.Models.ProductDetail>(StatusCodes.Status200OK)
-.Produces<ProductsApi.Models.ProductDetail>(StatusCodes.Status404NotFound)
-.WithName("GetProduct");
-
-app.MapPost("/products", async([FromBody]ProductsApi.Models.Product product, IProductRepository repository, CancellationToken cancellationToken) =>
-{
-    var newProduct = await repository.Add(product.Convert(), cancellationToken);
-    return Results.CreatedAtRoute($"/products/{newProduct.Id}", newProduct);
-})
-.Produces<ProductsApi.Models.Product>(StatusCodes.Status500InternalServerError)
-.Produces<ProductsApi.Models.Product>(StatusCodes.Status400BadRequest)
-.Produces<ProductsApi.Models.Product>(StatusCodes.Status201Created)
-.WithName("CreateProduct");
-
-app.MapPut("/products", async ([FromBody] ProductsApi.Models.Product product, IProductRepository repository, CancellationToken cancellationToken) =>
-{ 
-    var pdt = await repository.GetById(product.Id, cancellationToken);
-    if(pdt == null)
-    {
-        return Results.NotFound();
-    }
-    var updatedProduct = await repository.Update(product.Convert(), cancellationToken);
-    return Results.NoContent();
-})
-.Produces<ProductsApi.Models.Product>(StatusCodes.Status500InternalServerError)
-.Produces<ProductsApi.Models.Product>(StatusCodes.Status400BadRequest)
-.Produces<ProductsApi.Models.Product>(StatusCodes.Status204NoContent)
-.WithName("UpdateProduct");
-
-app.MapDelete("/products/{id:Guid}", async (Guid id, IProductRepository repository, CancellationToken cancellationToken) => {
-    var pdt = await repository.GetById(id, cancellationToken);
-    if (pdt == null)
-    {
-        return Results.NotFound();
-    }
-    await repository.Delete(id, cancellationToken);
-    return Results.NoContent();
-})
-.Produces<ProductsApi.Models.Product>(StatusCodes.Status500InternalServerError)
-.Produces<ProductsApi.Models.Product>(StatusCodes.Status400BadRequest)
-.Produces<ProductsApi.Models.Product>(StatusCodes.Status204NoContent)
-.WithName("DeleteProduct");
+app.RegisterProductEndpoints();
 
 app.Run();
